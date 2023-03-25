05077a55251d249444384df293fff9d49563c1df
jasmine_p <jasmine.pfingsten@gmail.com>
2023-03-24 22:09:03 -0400

    Set up starter files

---
+++ src/components/ReadingArea.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CardLayout from "CardLayout";
+
+function ReadingArea() {
+    <div className="reading-area">
+        <CardLayout />
+    </div>
+}
+
+export default ReadingArea;
