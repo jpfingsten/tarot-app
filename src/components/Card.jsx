75383f55805e803df4b60e5f9910240404ced8e6
jasmine_p <jasmine.pfingsten@gmail.com>
2023-03-24 22:09:48 -0400

    Set up starter files

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "React";
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="#" alt=""></img>
+        </div>
+    )
+}
+
+export default Card;
